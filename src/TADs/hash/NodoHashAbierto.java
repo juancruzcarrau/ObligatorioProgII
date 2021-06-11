3ddfcef87ecd4be54e1ddd0837f6d450a5261e15
Juan Cruz <juancruzcarrau1@gmail.com>
2021-06-11 16:27:29 -0300

    Se agrego una nueva TAD Hash.

---
+++ src/TADs/hash/NodoHashAbierto.java
@@ -0,0 +1,28 @@
+package TADs.hash;
+
+public class NodoHashAbierto<K, T>{
+
+    private K key;
+    private T data;
+
+    public NodoHashAbierto(K key, T data) {
+        this.key = key;
+        this.data = data;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
