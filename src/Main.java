9b4c6b3842662a858dcc50686a33d71948d51722
Juan Cruz <juancruzcarrau1@gmail.com>
2021-06-12 16:04:14 -0300

    Creacion de menu de consultas

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,6 @@ public class Main {
     static ListaEnlazada<CauseOfDeath> deathCauses = new ListaEnlazada<>();
     static ListaEnlazada<Movie> movies = new ListaEnlazada<>();
     static HashCerrado<String, Movie> moviesHash = new HashCerrado<>(86000,1);
-
     static ListaEnlazada<MovieCastMember> characters = new ListaEnlazada<>();
 
     public static void main(String[] args){
@@ -49,15 +48,60 @@ public class Main {
                 long startTime = System.currentTimeMillis();
                 cargarDatos();
                 long endTime = System.currentTimeMillis();
-                System.out.println("→ Tiempo de carga: " + (endTime - startTime) + " milisegundos");
+                System.out.println("Carga de datos exitosa, tiempo de ejecución de la carga:" + (endTime - startTime));
             } else if (seleccion == 2){
-//                ejectutarConsultas();
+                ejectutarConsultas();
             } else if (seleccion == 3){
                 break;
             } else {
                 throw new RuntimeException("Ha ocurrido un error. Seleccion no puede ser un nro distinto de 1, 2 o 3.");
             }
         }
+    }
+
+    private static void ejectutarConsultas() {
+
+        while (true) {
+            System.out.println("Seleccione la opción que desee:");
+            System.out.println("\t1. Indicar el Top 5 de actores/actrices que más apariciones han tenido a lo largo de los años.");
+            System.out.println("\t2. Indicar el Top 5 de las causas de muerte más frecuentes en directores y productores nacidos en Italia, Estados Unidos, Francia y UK.");
+            System.out.println("\t3. Mostrar de las 14 películas con más weightedAverage, el promedio de altura de sus actores/actrices si su valor es distinto de nulo.");
+            System.out.println("\t4. Indicar el año más habitual en el que nacen los actores y las actrices.");
+            System.out.println("\t5. Indicar el Top 10 de géneros de películas más populares, en las cuales al menos un actor/actriz tiene 2 o más hijos.");
+            System.out.println("\t6. Salir.");
+
+            System.out.print("Seleccion: ");
+
+            int seleccionConsulta;
+
+            try{
+                seleccionConsulta = scanner.nextInt();
+            } catch (InputMismatchException e){
+                System.out.println("Ingrese un numero. Intente de nuevo.");
+                scanner.next();
+                continue;
+            }
+
+            if(!Arrays.asList(1, 2, 3, 4, 5, 6).contains(seleccionConsulta)){
+                System.out.println("Seleccion incorrecta. Intente de nuevo.");
+                continue;
+            }
+
+            if(seleccionConsulta == 1){
+//                primeraConsulta();
+            } else if (seleccionConsulta == 2){
+//                segundaConsulta();
+            } else if (seleccionConsulta == 3){
+//                terceraConsulta();
+            } else if (seleccionConsulta == 4){
+//                cuartaConsulta();
+            } else if (seleccionConsulta == 5){
+//                quintaConsulta();
+            } else if (seleccionConsulta == 6){
+                break;
+            }
+        }
+
     }
 
     private static void cargarDatos() {
@@ -99,10 +143,6 @@ public class Main {
         }
 
         //Carga de castMembers y causas de muerte
-
-
-
-        // ANTERIOR CON CSV READER:
         try (CSVReader csvReader = new CSVReaderBuilder(new FileReader("dataset/IMDb names.csv")).withSkipLines(1).build()) {
             String[] valores = null;
 
